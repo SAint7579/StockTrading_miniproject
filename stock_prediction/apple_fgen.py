65ad2cdac1ce460308381d6ed5452a3de97ab74a
Vishwa <vishwa20@outlook.com>
2020-05-16 17:05:00 +0530

    Added Forecast

---
+++ stock_prediction/apple_fgen.py
@@ -0,0 +1,28 @@
+from keras.models import model_from_json
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+with open('Apple/apple_model.json','r') as f:
+    model = model_from_json(f.read())
+    
+model.load_weights('Apple/apple_w.h5')
+scaler = StandardScaler()
+dataset = pd.read_csv('aapl_msi_sbux.csv')
+
+#getting a random sample
+start = np.random.randint(1000,1030)
+sample = dataset.iloc[start:start+30,0].values
+sample = scaler.fit_transform(sample.reshape(-1,1))
+sample = sample.reshape(sample.shape[0],1,sample.shape[1])
+
+#Getting the forecast
+prediction = model.predict(sample)
+prediction = scaler.inverse_transform(prediction)
+
+dataframe = pd.DataFrame(list(enumerate(prediction.reshape(-1))),columns = ['Day','Price'])
+
+dataframe.to_csv("apple_forecast.csv")
+dataframe.to_json("apple_forecast.json")
